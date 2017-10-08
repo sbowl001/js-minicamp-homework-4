4e6a8bf80fe70752b695299f46f25c444a7f1d57
Stephanie <sbowl001@fiu.edu>
2017-10-08 18:03:40 +0800

    Solved exercises

--- exercises.js
+++ exercises.js
@@ -1,34 +1,67 @@
 // Do not change any of the function names
 
 function multiplyArguments() {
+  var product = 1;
+  if ( arguments.length === 0){
+  return 0; 
+  }
+  else if ( arguments.length === 1){
+   return arguments[0];
+  } else {
+   
+  for ( var i = 0; i < arguments.length; i++){
+  product *= arguments[i];
+  }
+  return product; 
+}
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
 }
 
 function invokeCallback(cb) {
-  // invoke cb
+  cb();// invoke cb
 }
 
 function sumArray(numbers, cb) {
-  // sum up all of the integers in the numbers array
+   var sum = numbers.reduce( function (runningTotal, num){
+   return runningTotal += num;
+   });
+   cb (sum);
+   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
 }
+//can also use forEach
 
 function forEach(arr, cb) {
-  // iterate over arr and pass its values to cb one by one
+  arr.forEach( function (value) {
+   return cb(value);
+  });// iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
 }
 
 function map(arr, cb) {
+  var newArray = arr.map( function( value){
+  return cb(value);
+  });
+  return newArray;
   // create a new array
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
   // the new array should be the same length as the array argument
 }
+// can also use for each & push newArray.push(cb(value));
 
 function getUserConstructor() {
-  // create a constructor called User
+  function User (options){
+  this.username = options.username;
+  this.password = options.password;
+  this.name = options.name;
+  this.email = options.email; 
+  this.sayHi = function() {
+   return  'Hello, my name is {{name}}';
+ };
+} return User;// create a constructor called User
   // it should accept an options object with username, name, email, and password properties
   // in the constructor set the username, name, email, and password properties
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
@@ -37,26 +70,48 @@ function getUserConstructor() {
 }
 
 function addPrototypeMethod(Constructor) {
-  // add a method to the constructor's prototype
+  Constructor.prototype.sayHi= function (){
+  return 'Hello World!';
+ };// add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
 }
 
 function addReverseString() {
-  // add a method to the string constructor's prototype that returns a reversed copy of the string
+  String.prototype.reverse =function (){
+  return this.split('').reverse().join('');
+  
+ };
+} // add a method to the string constructor's prototype that returns a reversed copy of the string
   // name this method reverse
   // hint:
   // you will need to use 'this' inside of reverse
-}
+
 
 function nFactorial(n) {
-  // return the factorial for n
+  if ( n===0) {
+  return 1;
+  } 
+  return n * nFactorial(n-1);
+   // return the factorial for n
   // solve this recursively
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
 }
 
 function cacheFunction(cb) {
-  // Extra Credit
+  var cache = {};
+  return function(value) {
+  if ( cache.hasOwnProperty(value)) { //hasOwnProperty is a boolean, whether object has specified property as own
+  return cache[value];
+  } else {
+  return cache[value] = cb(value);
+  }
+};
+}
+
+// no idea why the extra credit solution works, ask later when group is more active. 
+
+ // Extra Credit
   // use closure to create a cache for the cb function
   // the function that you return should accept a single argument and invoke cb with that argument
   // when the function you return is invoked with an argument it should save that argument and its result
@@ -67,7 +122,7 @@ function cacheFunction(cb) {
   // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
-}
+
 
 
 // Do not modify code below this line.
